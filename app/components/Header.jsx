9c849d7dfbf40838d4b6648954a27683a6da5064
Meirzhan <meirrr@MacBook-Air-Aikerim.local>
2024-06-05 22:58:55 +0500

    feat: change card on home page add shadows

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center px-[30px] py-[15px] shadow-md bg-gray-900 text-white lg:px-[100px] md:px-[100px]">
+    <header className="flex justify-between items-center px-[20px] py-[15px] shadow-md bg-gray-900 text-white lg:px-[100px] md:px-[100px]">
       <h2 className="text-2xl font-bold uppercase">meirrr&apos;s blog</h2>
       <nav className="text-sm">
         <ul className="flex space-x-3 lg:space-x-4 md:space-x-4">
--- app/page.js
+++ app/page.js
@@ -3,15 +3,23 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Meirrr&apos;s Blog</h1>
-      <p className="mb-6">
-        Discover interesting articles on various topics. Dive into our blog to
-        find out more!
-      </p>
-      <Link href="/blog" className="text-blue-500 hover:underline">
-        Go to Blog
-      </Link>
+    <div className="container mx-auto px-[5px] py-8">
+      <div className="container mx-auto shadow-md p-[15px] rounded-lg">
+        <button className="text-[10px] uppercase p-[3px] mb-[10px] shadow-md">
+          Adventure
+        </button>
+        <h2 className="text-2xl font-bold mb-2">
+          Welcome to meirrr&apos;s Blog
+        </h2>
+        <p className="mb-4 text-[12px]">
+          Discover interesting articles on various topics. Dive into our blog to
+          find out more!
+        </p>
+        <Link href="/blog" className="text-blue-500 hover:underline">
+          Go to Blog
+        </Link>
+      </div>
+      <div></div>
     </div>
   );
 }
