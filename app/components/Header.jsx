050c9a32e4ee825e9c27488e4b61e052530e4a35
Meirzhan <meirrr@MacBook-Air-Aikerim.local>
2024-06-05 22:26:51 +0500

    init: create homework project

---
+++ app/components/Header.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center px-[30px] py-[15px] shadow-md bg-gray-900 text-white lg:px-[100px] md:px-[100px]">
+      <h2 className="text-2xl font-bold uppercase">meirrr's blog</h2>
+      <nav className="text-sm">
+        <ul className="flex space-x-3 lg:space-x-4 md:space-x-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
