93ee00f38fc521ac2934be9c3764ed607f2819dc
Meirzhan <meirrr@MacBook-Air-Aikerim.local>
2024-06-05 22:31:25 +0500

    fix: implement correct apos

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Meirrr's Blog</h1>
+      <h1 className="text-4xl font-bold mb-4">Welcome to Meirrr&apos;s Blog</h1>
       <p className="mb-6">
         Discover interesting articles on various topics. Dive into our blog to
         find out more!
